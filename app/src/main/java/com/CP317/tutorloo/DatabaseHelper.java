9ae243a23a610cfc9d3a7f8b28b9a21a7a5c3d80
Anthony Youkhanna <a.youkh@hotmail.com>
2020-07-12 17:56:46 -0400

    Update DatabaseHelper.java

--- app/src/main/java/com/CP317/tutorloo/DatabaseHelper.java
+++ app/src/main/java/com/CP317/tutorloo/DatabaseHelper.java
@@ -1,18 +1,23 @@
 package com.CP317.tutorloo;
 
-import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.security.AccessControlContext;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "User.db";
     public static final String TABLE_NAME = "user_table";
-    public static final String COL_1 = "ID";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "EMAIL";
-    public static final String COL_4 = "PASSWORD";
+    public static final String COL_1 = "FIRST NAME";
+    public static final String COL_2 = "LAST NAME";
+    public static final String COL_3 = "DOB";
+    public static final String COL_4 = "EMAIL";
+    public static final String COL_5= "PASSWORD";
+    public static final String COL_6 = "ID";
 
-    public DatabaseHelper(Context context) {
+
+    public DatabaseHelper(AccessControlContext context) {
         super(context, DATABASE_NAME, null, 1);
 
     }
@@ -26,5 +31,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVer, int newVer) {
 
+    }
+
+    public boolean passAuth(String email, String password){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor mCursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE NAME='?' AND PASSWORD='?';",
+                new String[]{email,password});
     }
 }
