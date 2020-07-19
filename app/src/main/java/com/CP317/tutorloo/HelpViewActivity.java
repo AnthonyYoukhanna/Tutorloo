60fe60930f992d43ceedccde04483f8c8efa9a38
Anthony Youkhanna <a.youkh@hotmail.com>
2020-07-19 16:56:28 -0400

    Help Stuff
    made help icon and image button, added ability to navigate to help page and back from login screen

---
+++ app/src/main/java/com/CP317/tutorloo/HelpViewActivity.java
@@ -0,0 +1,14 @@
+package com.CP317.tutorloo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HelpViewActivity extends AppCompatActivity {
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_view);
+    }
+
+}
--- app/src/main/java/com/CP317/tutorloo/TutorActivity.java
+++ app/src/main/java/com/CP317/tutorloo/TutorActivity.java
@@ -9,20 +9,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorActivity extends AppCompatActivity {
 
-    private Button mInfo;
+    private Button mTutorInfo;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorview);
 
-        mInfo = (Button) findViewById(R.id.change_profile);
+        mTutorInfo = (Button) findViewById(R.id.change_profile);
 
-        mInfo.setOnClickListener(new View.OnClickListener() {
+        mTutorInfo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(TutorActivity.this, TutorInfoActivity.class);
                 startActivity(intent);
-                finish();
                 return;
             }
         });
