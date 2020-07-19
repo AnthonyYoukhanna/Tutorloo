64efbbdd9f162839eb522d16aa0d2c78a149a6db
Regina Khalil <khal4400@mylaurier.ca>
2020-07-19 15:38:43 -0400

    register view added. student view added but needs to be completed

--- app/src/main/java/com/CP317/tutorloo/LoginActivity.java
+++ app/src/main/java/com/CP317/tutorloo/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected  void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_LoginView);
 
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
