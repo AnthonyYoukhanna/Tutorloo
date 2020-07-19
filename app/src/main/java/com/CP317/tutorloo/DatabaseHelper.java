2576d2c8706e64f18b628dc0d4c1469e6c0b5d43
Anthony Youkhanna <a.youkh@hotmail.com>
2020-07-19 15:46:09 -0400

    Update DatabaseHelper.java

--- app/src/main/java/com/CP317/tutorloo/DatabaseHelper.java
+++ app/src/main/java/com/CP317/tutorloo/DatabaseHelper.java
@@ -1,10 +1,9 @@
 package com.CP317.tutorloo;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-
-import java.security.AccessControlContext;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "User.db";
@@ -17,7 +16,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_6 = "ID";
 
 
-    public DatabaseHelper(AccessControlContext context) {
+    public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
 
     }
@@ -37,5 +36,14 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor mCursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE NAME='?' AND PASSWORD='?';",
                 new String[]{email,password});
+
+        if (mCursor != null) {
+            if(mCursor.getCount() > 0)
+            {
+                mCursor.close();
+                return true;
+            }
+        }
+        return false;
     }
 }
