db6b8922d794ba7620f778a5ad57edc115394c3f
divyan <65295832+divyan@users.noreply.github.com>
2020-07-22 19:01:43 -0400

    Added back button functionality in StudentRegisterActivity

--- app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
@@ -3,19 +3,23 @@ package com.CP317.tutorloo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentRegisterActivity extends AppCompatActivity {
 
+    private ImageButton mInfo;
     private ImageButton mPrevious;
+
     @Override
-    protected  void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentregisterview);
 
-        mPrevious = (ImageButton) findViewById(R.id.RegisterPrevious);
+        mInfo = (ImageButton) findViewById(R.id.helpButton);
+        mPrevious = (ImageButton) findViewById(R.id.previous2);
 
         mPrevious.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -24,5 +28,14 @@ public class StudentRegisterActivity extends AppCompatActivity {
                 return;
             }
         });
+
+        mInfo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentRegisterActivity.this, HelpViewActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
+
     }
 }
