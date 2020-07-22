4d9f5a6e449859ccccbca084639719c3e611bb7d
Regina Khalil <khal4400@mylaurier.ca>
2020-07-22 18:17:08 -0400

    StudentRegisterActivity: navigation to previous page added. studentregisterview: white header removed. dimensions and constraints fixed.

--- app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
@@ -1,12 +1,28 @@
 package com.CP317.tutorloo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentRegisterActivity extends AppCompatActivity {
+
+    private ImageButton mPrevious;
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentregisterview);
+
+        mPrevious = (ImageButton) findViewById(R.id.studentRegisterPrevious);
+
+        mPrevious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
     }
 }
