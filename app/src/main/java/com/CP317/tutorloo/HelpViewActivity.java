d41e9e34112f95821cabf293c26f1cdf5c4d727d
Regina Khalil <khal4400@mylaurier.ca>
2020-07-22 17:00:33 -0400

    HelpViewActivity: 'previous' button is implemented HelpView: white header removed. dimensions updated

--- app/src/main/java/com/CP317/tutorloo/HelpViewActivity.java
+++ app/src/main/java/com/CP317/tutorloo/HelpViewActivity.java
@@ -1,14 +1,28 @@
 package com.CP317.tutorloo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HelpViewActivity extends AppCompatActivity {
+    private ImageButton mPrevious;
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_helpview);
+
+        mPrevious = (ImageButton) findViewById(R.id.help_previous);
+
+        mPrevious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(HelpViewActivity.this, LoginActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
     }
 }
 
