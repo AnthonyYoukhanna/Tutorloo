a2d50ba5d70053846d1497ad9c27a72c8718ae08
Regina Khalil <khal4400@mylaurier.ca>
2020-07-23 14:10:04 -0400

    navigation from studentregisterview to studentview fixed

--- app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
@@ -4,22 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentRegisterActivity extends AppCompatActivity {
 
     private ImageButton mPrevious;
+    private Button mSubmit;
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentregisterview);
 
         mPrevious = (ImageButton) findViewById(R.id.RegisterPrevious);
+        mSubmit = (Button) findViewById(R.id.submit);
 
         mPrevious.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(StudentRegisterActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentRegisterActivity.this, StudentActivity.class);
                 startActivity(intent);
                 return;
             }
