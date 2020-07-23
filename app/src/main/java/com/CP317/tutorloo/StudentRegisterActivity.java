59766c73d85d6a0d3b4529c0586d54ad37dbb92c
Regina Khalil <khal4400@mylaurier.ca>
2020-07-22 22:28:00 -0400

    Added button functionality to the student's 'Submit' and the tutor's 'Continue'

--- app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentRegisterActivity.java
@@ -3,6 +3,7 @@ package com.CP317.tutorloo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,15 @@ import androidx.appcompat.app.AppCompatActivity;
 public class StudentRegisterActivity extends AppCompatActivity {
 
     private ImageButton mPrevious;
+    private Button mSubmit;
+
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentregisterview);
 
         mPrevious = (ImageButton) findViewById(R.id.RegisterPrevious);
+        mSubmit = (Button) findViewById(R.id.submit);
 
         mPrevious.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -24,5 +28,14 @@ public class StudentRegisterActivity extends AppCompatActivity {
                 return;
             }
         });
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentRegisterActivity.this, StudentActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
+
     }
 }
