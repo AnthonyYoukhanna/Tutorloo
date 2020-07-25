e71261502b4b6fd62a2e11082d9d6cb1536511a4
Regina Khalil <khal4400@mylaurier.ca>
2020-07-25 13:32:24 -0400

    navigation to previous page added to resetpasswordview

--- app/src/main/java/com/CP317/tutorloo/ResetPasswordActivity.java
+++ app/src/main/java/com/CP317/tutorloo/ResetPasswordActivity.java
@@ -1,15 +1,32 @@
 package com.CP317.tutorloo;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResetPasswordActivity extends AppCompatActivity {
+
+    private ImageView mPrevious;
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpassword);
+
+        mPrevious = (ImageView) findViewById(R.id.resetPrevious);
+
+        mPrevious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(ResetPasswordActivity.this, LoginActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
 
     }
 }
