3421b64d2bc1ae43c3a5932ecdf7c5deea8e292b
Regina Khalil <khal4400@mylaurier.ca>
2020-07-24 22:17:39 -0400

    tutorview layout fixed: constraints and dimensions

--- app/src/main/java/com/CP317/tutorloo/TutorActivity.java
+++ app/src/main/java/com/CP317/tutorloo/TutorActivity.java
@@ -10,35 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class TutorActivity extends AppCompatActivity {
 
-//    private Button mTutorInfo;
-//    private ImageButton mInfo;
-
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorview);
-
-//        mTutorInfo = (Button) findViewById(R.id.change_profile);
-//        mInfo = (ImageButton) findViewById(R.id.HelpButton);
-//
-//        mTutorInfo.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View view) {
-//                Intent intent = new Intent(TutorActivity.this, TutorInfoActivity.class);
-//                startActivity(intent);
-//                return;
-//            }
-//        });
-
-
-//        mInfo.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View view) {
-//                Intent intent = new Intent(TutorActivity.this, HelpViewActivity.class);
-//                startActivity(intent);
-//                return;
-//            }
-//        });
-
     }
 
 
