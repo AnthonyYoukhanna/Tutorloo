8ae9a28a0656e19cc4d5655a0766c7e1c9d9c0a6
Regina Khalil <khal4400@mylaurier.ca>
2020-07-25 20:29:11 -0400

    popup menu added to studentview and tutorview: include view profile and logout options

--- app/src/main/java/com/CP317/tutorloo/StudentActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentActivity.java
@@ -1,17 +1,41 @@
 package com.CP317.tutorloo;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.PopupMenu;
+import android.view.View;
+import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StudentActivity extends AppCompatActivity {
+public class StudentActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentview);
 
 
+    }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.profile_popup);
+        popup.show();
+    }
 
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile:
+                Toast.makeText(this, "Profile button clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                Toast.makeText(this, "Logout button clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 }
--- app/src/main/java/com/CP317/tutorloo/TutorActivity.java
+++ app/src/main/java/com/CP317/tutorloo/TutorActivity.java
@@ -2,19 +2,43 @@ package com.CP317.tutorloo;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TutorActivity extends AppCompatActivity {
+public class TutorActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
 
 
     @Override
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorview);
+    }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.profile_popup);
+        popup.show();
+    }
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.profile:
+                Toast.makeText(this, "Profile button clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                Toast.makeText(this, "Logout button clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
     }
 
 
