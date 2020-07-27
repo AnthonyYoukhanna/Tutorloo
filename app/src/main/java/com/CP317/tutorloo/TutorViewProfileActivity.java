128056df1065ec7e9f591432f84c54d64b00ae37
Yvonne Itangishaka <itan3040@mylaurier.ca>
2020-07-27 18:12:20 -0400

    Tutor's profile (tutor's perspective)

---
+++ app/src/main/java/com/CP317/tutorloo/TutorViewProfileActivity.java
@@ -0,0 +1,13 @@
+package com.CP317.tutorloo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TutorViewProfileActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorviewprofile);
+    }
+}
