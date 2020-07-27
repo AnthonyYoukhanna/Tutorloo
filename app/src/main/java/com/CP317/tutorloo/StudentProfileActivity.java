555aad9e7e8b912474fe774168c5a36400f6c189
Regina Khalil <khal4400@mylaurier.ca>
2020-07-27 14:23:35 -0400

    navigation to previous view added on StudentProfileActivity and studentprofileview

--- app/src/main/java/com/CP317/tutorloo/StudentProfileActivity.java
+++ app/src/main/java/com/CP317/tutorloo/StudentProfileActivity.java
@@ -7,20 +7,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class StudentProfileActivity extends AppCompatActivity {
 
     private Button mEditProfile;
+    private ImageButton mPrevious;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_studentprofileview);
 
         mEditProfile = (Button) findViewById(R.id.Edit);
+        mPrevious = (ImageButton) findViewById(R.id.studentPrevious);
 
         mEditProfile.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent(StudentProfileActivity.this, EditProfileStudentActivity.class);
+                startActivity(intent);
+                return;
+            }
+        });
+
+        mPrevious.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent(StudentProfileActivity.this, StudentActivity.class);
                 startActivity(intent);
                 return;
             }
