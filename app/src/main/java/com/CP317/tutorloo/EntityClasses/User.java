83d54ce8aaaf0d8b7acf3ce153f3addec28bc9ba
Regina Khalil <khal4400@mylaurier.ca>
2020-08-01 15:53:17 -0400

    Mutator and accessors added to user entity class

--- app/src/main/java/com/CP317/tutorloo/EntityClasses/User.java
+++ app/src/main/java/com/CP317/tutorloo/EntityClasses/User.java
@@ -1,6 +1,6 @@
 package com.CP317.tutorloo.EntityClasses;
 
-public class User  {
+public class User {
 
     private String firstName;
     private String lastName;
@@ -9,26 +9,68 @@ public class User  {
     private String password;
     private int userID;
 
-    //Constructor
-    public void CreateProfile() {
-
+    //-----first name accessor and mutators-------
+    public String getfirstName() {
+        return this.firstName;
     }
 
-    public boolean RemoveProfile(int userid) {
-        return false;
+    public void setFirstName(String firstName) {
+        firstName = this.firstName;
     }
 
-    public void UpdateProfile(int userid) {
+    //-------------------------------------------
 
+
+    //-----last name accessor and mutators-------
+    public String getlastName() {
+        return this.lastName;
     }
 
-    public boolean login(String email, String password) {
-        return false;
+    public void setLastName(String lastName) {
+        lastName = this.lastName;
+    }
+    //--------------------------------------------
+
+
+    //-----Email accessor and mutators-------
+    public String email() {
+        return this.email;
     }
 
-    public boolean logout(String email, String password) {
-        return false;
+    public void setEmail(String email) {
+        email = this.email;
     }
 
+    public String getdob() {
+        return this.dob;
+    }
+
+    //---------------------------------------
+
+    //-----Password accessor and mutators-------
+
+    public void setPassword(String password) {
+        password = this.password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+    //----------------------------------------
+
+
+    //-----User ID accessor and mutators-------
+    public void setUserID(int userID) {
+        userID = this.userID;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
 
 }
+
+
+
+
+
