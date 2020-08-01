d85a9411e2de950a7f39143e9e2650935ac7ff6c
Regina Khalil <khal4400@mylaurier.ca>
2020-08-01 16:00:10 -0400

    accessors and mutators added to tutor entity class

--- app/src/main/java/com/CP317/tutorloo/EntityClasses/Tutor.java
+++ app/src/main/java/com/CP317/tutorloo/EntityClasses/Tutor.java
@@ -8,8 +8,55 @@ public class Tutor extends User {
     private int year_of_study;
     private String biography;
 
-    private void UpdateTutorProfile(int userid) {
-
+    //-----------hourly rate accessor and mutator---------
+    public void setHourlyRate(double hourlyRate) {
+        hourlyRate = this.hourlyRate;
     }
+
+    public double getHourlyRate() {
+        return this.hourlyRate;
+    }
+    //--------------------------------------------------
+
+
+    //ADD course accessor and mutator!!
+//    public void setCourses(String[] courses) {
+//        courses[] = this.courses;
+//    }
+
+    //-----------program accessor and mutator---------
+    public void setProgram(String program) {
+        program = this.program;
+    }
+
+    public String getProgram() {
+        return this.program;
+    }
+    //-----------------------------------------------
+
+    //-----------year of study accessor and mutator---------
+
+    public void setYear_of_study(int year_of_study) {
+        year_of_study = this.year_of_study;
+    }
+
+    public int getYear_of_study() {
+        return this.year_of_study;
+    }
+
+    //---------------------------------------------
+
+    //-----------Bio accessor and mutator---------
+    public void setBiography(String biography) {
+        biography = this.biography;
+    }
+
+    public String getBiography() {
+        return this.biography;
+    }
+    //--------------------------------------------
+//    private void UpdateTutorProfile(int userid) {
+//
+//    }
 
 }
