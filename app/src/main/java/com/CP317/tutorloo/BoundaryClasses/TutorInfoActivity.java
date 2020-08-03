0447f3b8c9e16b49c4626d4f9944ffdeb4e8d5b6
Regina Khalil <khal4400@mylaurier.ca>
2020-08-03 18:09:30 -0400

    hid keyboard functions added to some views

--- app/src/main/java/com/CP317/tutorloo/BoundaryClasses/TutorInfoActivity.java
+++ app/src/main/java/com/CP317/tutorloo/BoundaryClasses/TutorInfoActivity.java
@@ -1,10 +1,13 @@
 package com.CP317.tutorloo.BoundaryClasses;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
 import android.widget.Button;
 import android.widget.EditText;
@@ -49,6 +52,16 @@ public class TutorInfoActivity extends AppCompatActivity {
                 return;
             }
         });
+    }
+
+    //Hides keyboard when clicking off edit text box
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 
     //validate information
--- app/src/main/java/com/CP317/tutorloo/BoundaryClasses/TutorProfileActivity.java
+++ app/src/main/java/com/CP317/tutorloo/BoundaryClasses/TutorProfileActivity.java
@@ -1,6 +1,10 @@
 package com.CP317.tutorloo.BoundaryClasses;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,10 +12,20 @@ import com.CP317.tutorloo.R;
 
 public class TutorProfileActivity extends AppCompatActivity {
 
+    private Button mEdit;
+
+
     protected  void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorprofileview);
+        boolean tutor;
+        mEdit = (Button) findViewById(R.id.edit_profile);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        tutor = sharedPref.getBoolean("Student", false);
 
+        if (tutor){
+            mEdit.setVisibility(View.VISIBLE);
+        }
 
     }
 }
